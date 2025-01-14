ef25ecfdab55601937d63a6c08404756fe0f5130
Emily Han <emilyhan1011@gmail.com>
2025-01-14 00:28:09 -0500

    changed vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,8 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/blog/",  //For githubpages hosting, put URL path name for our website as base of our defineConfig.
-        //At the moment, we are not buying a domain name, so need to define same as repository name
+  base: "/",  //For githubpages hosting, put URL path name for our website as base of our defineConfig.
+        //At the moment, we are not buying a domain name, so need to define same as repository name 
+        //for github pages: /blog/
+        //for Render /
   build: {
       outDir: 'dist' // Ensure the output directory matches the deploy script
   }
